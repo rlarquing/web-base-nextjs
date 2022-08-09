cd90c5bf8fcbc639b17b40289884af9a2c2e146d
Reynelbis Larquin Guerra <reynelbis@camaguey.geocuba.cu>
2022-08-08 23:06:42 -0400

    commit inicial

---
+++ redux/states/user.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const UserEmptyState: any = {
+  name: '',
+  gender: '',
+  status: ''
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: UserEmptyState,
+  reducers: {
+    createUser: (state, action) => action.payload,
+    modifyUser: (state, action) => ({ ...state, ...action.payload }),
+    resetUser: () => UserEmptyState
+  }
+});
+
+export const { createUser, modifyUser, resetUser } = userSlice.actions;
+
+export default userSlice.reducer;
