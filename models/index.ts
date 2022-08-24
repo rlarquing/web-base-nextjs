7f23cab5269c2abdf7eda7b60e01a71737d99082
Reynelbis Larquin Guerra <reynelbis@camaguey.geocuba.cu>
2022-08-24 19:11:42 -0400

    reorganizando las carpetas y sus ficheros para ponerle un sufijo

--- models/index.ts
+++ models/index.ts
@@ -2,3 +2,4 @@ export * from './axios-call.model';
 export * from './regex.model';
 export * from './select-options.model';
 export * from './subject-manager.model';
+export * from './action.model';
