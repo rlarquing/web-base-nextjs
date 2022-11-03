edbd310e00bd27403e5d126cb1b15b0f54fa33f5
Reynelbis Larquin Guerra <reynelbis@camaguey.geocuba.cu>
2022-11-03 17:50:07 -0400

    listo la funcion que va a llamar al enpoint de busqueda

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -3,20 +3,23 @@ import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
+import {useEffect, useState} from "react";
 interface SearchInputProps {
     id: string,
     placeholder: string,
-    buscar: () => void,
+    buscar: (value:string) => void,
 }
 export function SearchInput({id, placeholder, buscar}: SearchInputProps) {
+    const [value, setValue] = useState('');
     return (
         <Paper elevation={0}>
             <InputBase
                 id={id}
                 placeholder={placeholder}
                 inputProps={{ 'aria-label': placeholder }}
+                onChange={(e)=>setValue(e.target.value)}
             />
-            <IconButton type="button" aria-label="search" onClick={buscar}>
+            <IconButton type="button" aria-label="search" onClick={()=>buscar(value)}>
                 <SearchIcon/>
             </IconButton>
         </Paper>
--- pages/administration/users/index.tsx
+++ pages/administration/users/index.tsx
@@ -25,8 +25,8 @@ export default function Index({data}: any) {
             }
         }
 
-        const buscar = ()=>{
-            console.log('hola');
+        const buscar = (value:string)=>{
+            console.log(value);
         }
 
         useEffect(()=>{
